ecd8535e3e373a802b8575ea6a3ddbc4a487abd1
Bogdan Liashuk <Bodyabody2408@gmail.com>
2023-11-21 15:36:38 +0200

    Last version HW 1

---
+++ JS1/HWJS1.js
@@ -0,0 +1,39 @@
+let st = 'Hello';
+console.log(st)
+let o = 'owu';
+console.log(o)
+let d = 'com';
+console.log(d)
+let country = 'ua';
+console.log(country)
+let n = 1;
+console.log(n)
+let ten = 10;
+console.log(ten)
+let nine = -999;
+console.log(nine)
+let one = 123;
+console.log(one)
+let p = 3.14;
+console.log(p)
+let two = 2.7;
+console.log(two)
+let six = 16;
+console.log(six)
+let t = 5 == 5;
+console.log(t)
+let f = 5 < 5;
+console.log(f)
+
+let firstName = 'Богдан';
+let middleName = 'Богданович';
+let lastName = 'Ляшук';
+
+let person = firstName + middleName + lastName
+
+let  a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof b)
+let c = true;
+console.log(typeof c)
